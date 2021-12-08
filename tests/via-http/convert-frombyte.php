7071d5f0844ceede6bb904ebad79c2d132fc3931
ve3 <kawewong@gmail.com>
2021-12-08 15:31:28 +0700

    Typo fix.

--- tests/via-http/convert-frombyte.php
+++ tests/via-http/convert-frombyte.php
@@ -3,7 +3,9 @@
 require dirname(dirname(__DIR__)).'/Rundiz/Number/Number.php';
 
 $filesizes = array(
-    100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000, 10000000000, 200000000000, 1000000000000, 20000000000001, 100000000000000, 3000000000000000, 10000000000000000, 100000000000000000, 1000000000000000000, 10000000000000000000,
+    100, 1000, 10000, 100000, 1000000, 
+    10000000, 100000000, 1000000000, 10000000000, 200000000000, 1000000000000, 
+    20000000000001, 100000000000000, 3000000000000000, 10000000000000000, 100000000000000000, 1000000000000000000, 10000000000000000000,
     130, 1300, 13300, 133300, 1333300, 13333300, 133333300, 1333333300, 13333333300, 133333333300, 1333333333300, 13333333333300,
 );
 
@@ -17,31 +19,31 @@ foreach ($filesizes as $size) {
 }
 echo '<br>';
 
-echo '<strong>convert from bytes KB unit</strong><br>';
+echo '<strong>convert from bytes to KB unit</strong><br>';
 foreach ($filesizes as $size) {
     echo $size.' = '.$number->fromBytes($size, 'KB').'<br>';
 }
 echo '<br>';
 
-echo '<strong>convert from bytes KiB unit</strong><br>';
+echo '<strong>convert from bytes to KiB unit</strong><br>';
 foreach ($filesizes as $size) {
     echo $size.' = '.$number->fromBytes($size, 'KiB').'<br>';
 }
 echo '<br>';
 
-echo '<strong>convert from bytes GB unit</strong><br>';
+echo '<strong>convert from bytes to GB unit</strong><br>';
 foreach ($filesizes as $size) {
     echo $size.' = '.$number->fromBytes($size, 'GB').'<br>';
 }
 echo '<br>';
 
-echo '<strong>convert from bytes YB unit</strong><br>';
+echo '<strong>convert from bytes to YB unit</strong><br>';
 foreach ($filesizes as $size) {
     echo $size.' = '.$number->fromBytes($size, 'YB').'<br>';
 }
 echo '<br>';
 
-echo '<strong>convert from bytes ERROR unit</strong><br>';
+echo '<strong>convert from bytes to ERROR unit</strong><br>';
 foreach ($filesizes as $size) {
     echo $size.' = '.$number->fromBytes($size, 'ERROR').'<br>';
 }
